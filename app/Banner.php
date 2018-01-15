3358af29d60c7b326976d55fc837d1ca82b3a8ac
Dmitriy Shcherbakov <shcherbakov.dmytriy@gmail.com>
2018-01-15 13:00:58 +0200

    remove unused docs

--- app/Banner.php
+++ app/Banner.php
@@ -5,30 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-/**
- * App\Banner
- *
- * @property int $id
- * @property string|null $name
- * @property string $image
- * @property \Carbon\Carbon|null $created_at
- * @property \Carbon\Carbon|null $updated_at
- * @property string|null $deleted_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereImage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereUpdatedAt($value)
- * @mixin \Eloquent
- * @property int $isActive
- * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Query\Builder|\App\Banner onlyTrashed()
- * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Banner whereIsActive($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Banner withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Banner withoutTrashed()
- */
 class Banner extends Model
 {
     use SoftDeletes;
--- app/Type.php
+++ app/Type.php
@@ -5,22 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-/**
- * App\Type
- *
- * @property int $id
- * @property string $name
- * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Query\Builder|\App\Type onlyTrashed()
- * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Type whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Type whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Type withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Type withoutTrashed()
- * @mixin \Eloquent
- * @property \Carbon\Carbon|null $deleted_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Type whereDeletedAt($value)
- */
 class Type extends Model
 {
     use SoftDeletes;

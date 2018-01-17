0dffc41854fa1622cbb3594eddd93cd54339467b
Dmitriy Shcherbakov <shcherbakov.dmytriy@gmail.com>
2018-01-17 17:18:11 +0200

    add about us page

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.main')
+
+@section('body')
+
+<div class="about-container">
+    <div class="image-text-holder space-btw">
+        <div class="image-holder"><img src="img/kotl_info.jpg" alt="" width="100%"></div>
+
+        <div class="text-holder">
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</span>
+        </div>
+    </div>
+</div>
+
+<div class="advantages-container space-btw">
+    <div class="advantage-card advantage-card-1">
+        <div class="table-texture">
+            <div><span>15 лет успешной работы</span></div>
+        </div>
+    </div>
+    <div class="advantage-card advantage-card-2">
+        <div class="table-texture">
+            <div><span>Более 50 партнёров</span></div>
+        </div>
+    </div>
+    <div class="advantage-card advantage-card-3">
+        <div class="table-texture">
+            <div><span>Более 5 000 клиентов</span></div>
+        </div>
+    </div>
+    <div class="advantage-card advantage-card-4">
+        <div class="table-texture">
+            <div><span>Гибкое ценообразование</span></div>
+        </div>
+    </div>
+    <div class="advantage-card advantage-card-5">
+        <div class="table-texture">
+            <div><span>Команда профессионалов</span></div>
+        </div>
+    </div>
+    <div class="advantage-card advantage-card-6">
+        <div class="table-texture">
+            <div><span>Индивидуальный подход</span></div>
+        </div>
+    </div>
+</div>
+
+@stop
